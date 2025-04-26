fb7f71a1df22b4880f6c5f5cfe38787c61247c92
aryandumale04 <aryandumale04@gmail.com>
2025-04-26 17:29:42 +0530

    Add Day2{Project2} with home ,create and delete route

---
+++ Backend/MongoDB/Day2/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path= require("path");
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+const userModel = require("./models/user");
+
+app.get('/',(req,res)=>{
+
+    res.render("index");
+
+});
+app.get('/read',async (req,res)=>{
+    let allUSers= await userModel.find();
+    res.render("read",{users : allUSers});
+
+});
+app.post('/create', async (req,res)=>{
+    let {name, email, image} =req.body;
+   let createdUser = await  userModel.create({
+
+        name : name,
+        email : email,
+        image
+
+    })
+    res.redirect("/read");
+    
+
+});
+app.get('/delete/:id',async (req,res)=>{
+   let deletedUSer= await  userModel.findOneAndDelete({_id : req.params.id});
+   res.redirect("/read");
+});
+
+app.listen(3000);
+
