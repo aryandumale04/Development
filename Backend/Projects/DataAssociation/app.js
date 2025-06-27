90c63d7a87ebf0287c8c35158b76b1d2f4bc0cf2
aryandumale04 <aryandumale04@gmail.com>
2025-06-27 16:27:12 +0530

    Added Protected route PROFILE

--- Backend/Projects/DataAssociation/app.js
+++ Backend/Projects/DataAssociation/app.js
@@ -7,6 +7,7 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt =  require("bcrypt");
 const jwt =  require("jsonwebtoken");
+const user = require("./models/user");
 
 
 
@@ -20,6 +21,10 @@ app.use(cookieParser());// so that we can read cookie
 //default Home route
 app.get("/" ,(req,res) => {
     res.render("index");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login")
 })
 
 // create route 
@@ -43,6 +48,7 @@ app.post("/register" , async (req,res)=>{
                 name,
                 age,
                 password : hash,
+                email 
 
             });
             //user aagay ab issse login karwao
@@ -60,6 +66,58 @@ app.post("/register" , async (req,res)=>{
    };
    
 })
+app.get("/profile" ,isLoggedIn, (req,res)=>{
+    console.log(req.user);
+
+})
+
+app.post("/login" ,  async (req,res)=>{
+    //check if already toh nahi
+   let {email, password } = req.body; 
+   let user = await userModel.findOne({email
+    // iska matlab hai email : email 
+    
+   }) ;
+   if(!user){
+        res.send("Something went Wrong!!");
+    }
+    else{
+        bcrypt.compare(password,user.password , function(err,result){
+            if(!result){
+                res.send("Something went wrong with password");
+
+            }
+            else{
+                let token =  jwt.sign({email: email,userid: user._id} ,"shhhhhh");
+                res.cookie("token",token);
+                res.status(200).send("you can login");
+                
+            }
+
+        });
+
+    }
+  
+   
+})
+
+app.get("/logout" ,(req,res)=>{
+    res.cookie("token","");
+
+    res.redirect("/login");
+})
+function isLoggedIn(req,res,next){
+    if(req.cookies.token === ""){
+        res.send("You ,must be login");
+    }
+    else{
+       let data =  jwt.verify(req.cookies.token,"shhhhhh");
+       req.user = data;
+       next();
+    }
+    
+    
+}
 
 
 app.listen(3000);
